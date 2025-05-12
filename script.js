48435fa9921a8bc4e5ded6eec2e9e4de38b8864a
adrlod747 <adrlod747@edu.linkoping.se>
2025-05-12 12:57:24 +0000

    sadsd

---
+++ script.js
@@ -0,0 +1,14 @@
+const annoying = document.querySelector(".annoying");
+const Button = document.querySelector(".Button");
+const cookie = document.querySelector(".cookie");
+
+Button.addEventListener("click", function (event) {
+    annoying.style.display = "none";
+  
+    console.log("hej");
+  });
+  Button.addEventListener("click", function (event) {
+    cookie.style.display = "none";
+  
+    console.log("hej");
+  });
