15a35948333a1df565b7889e77893c9a69e4e3c4
adrlod747 <adrlod747@edu.linkoping.se>
2025-05-22 17:31:22 +0000

    3 timmar arbete

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const annoying = document.querySelector(".annoying");
 const Button = document.querySelector(".Button");
+const Button2 = document.querySelector(".Button2");
 const cookie = document.querySelector(".cookie");
 
 Button.addEventListener("click", function (event) {
     annoying.style.display = "none";
-  
-    console.log("hej");
   });
-  Button.addEventListener("click", function (event) {
+Button.addEventListener("click", function (event) {
     cookie.style.display = "none";
-  
-    console.log("hej");
+  });
+Button2.addEventListener("click", function (event) {
+    annoying.style.display = "none";
+  });
+Button2.addEventListener("click", function (event) {
+    cookie.style.display = "none";
   });
