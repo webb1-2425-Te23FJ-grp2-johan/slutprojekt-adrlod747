3fb247a0f2f38221df22ef21f3545765ad199695
adrlod747 <adrlod747@edu.linkoping.se>
2025-06-03 08:14:20 +0000

    sss

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const annoying = document.querySelector(".annoying");
 const Button = document.querySelector(".Button");
 const Button2 = document.querySelector(".Button2");
 const cookie = document.querySelector(".cookie");
+const kaka = document.querySelector(".kaka");
 
 Button.addEventListener("click", function (event) {
     annoying.style.display = "none";
     cookie.style.display = "none";
+    kaka.style.display = "none";
   });
 
 Button2.addEventListener("click", function (event) {
     annoying.style.display = "none";
     cookie.style.display = "none";
+    kaka.style.display = "none";
   });
